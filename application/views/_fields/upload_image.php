5d8303118959d15a0f50be8c4e85d69f20ee5391
spelka <sebastian.pelka@hotmail.com>
2015-02-19 20:30:50 -0800

    Created a image upload form widget, and a date picket form widget.
    Signed-off-by: spelka <sebastian.pelka@hotmail.com>

---
+++ application/views/_fields/upload_image.php
@@ -0,0 +1,20 @@
+<div class="control-group">
+    <label class="control-label" for="{name}">{label}</label>
+    <div class="controls">
+        <input type="file" id="{name}" name="{name}" value="{value}" maxLength="{maxlen}" style="width:{size}em;height:2em" {disabled}/>
+               <br/><small><em>{explain}</em></small>
+		<br /><br />
+		<input type="submit" value="upload" />
+
+    </div>
+</div>
+
+
+
+
+
+<input type="file" name="userfile" size="20" />
+
+<br /><br />
+
+<input type="submit" value="upload" />
